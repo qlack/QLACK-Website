6bad1bcf4d7a5cc9a59cba171143b7eb2f659895
sotikele <xsotos@hotmail.com>
2019-10-02 15:39:26 +0300

    added scroll to top button ,design changes

--- assets/js/versions.js
+++ assets/js/versions.js
@@ -9,20 +9,12 @@ xhr.onreadystatechange = function () {
     
      var myObj = JSON.parse(xhr.responseText);
      for (x in myObj) {
-       if(x%2===0){
+      
         text+=  "<section class='no-border-bottom section-sm'><div class='container'> <header class='section-header text-left'>";
-        text += " <span>" + myObj[x].published_at.slice(0,10) + "</span>";
+        text += " <span>" + myObj[x].created_at.slice(0,10) + "</span>";
         text +=  "<h2>"+myObj[x].name+"</h2>"
         text +=  "<p class='version-body'>"+ myObj[x].body+"</p>"
         text += "  </header> </div> </section>";
-      }
-      else{
-        text+=  "<section class='no-border-bottom section-sm '><div class='container'> <header class='section-header text-left'>";
-        text += " <span>" + myObj[x].published_at.slice(0,10) + "</span>";
-        text +=  "<h2>"+myObj[x].name+"</h2>"
-        text +=  "<p class='version-body'>"+ myObj[x].body+"</p>"
-        text += "  </header> </div> </section>";
-      }
    }
     
     document.getElementById("versions").innerHTML = text;
